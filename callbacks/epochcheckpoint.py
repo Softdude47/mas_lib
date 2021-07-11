7728930a16691c0b5d741f43cba15f87ae50d155
Godwin Precious <godwinprecious47@gmail.com>
2021-07-11 08:57:08 +0100

    added optional string formatting

--- callbacks/epochcheckpoint.py
+++ callbacks/epochcheckpoint.py
@@ -1,13 +1,42 @@
+
+import os
+from pathlib import Path
 from tensorflow.keras.callbacks import BaseLogger
 
 class EpochCheckpoint(BaseLogger):
-    def __init__(self, interval, path):
+    def __init__(self, path, interval):
         # initialize base class and store parameters
         super(EpochCheckpoint, self).__init__()
         self.interval = interval
         self.path = path
         
     def on_epoch_end(self, epoch, logs={}):
+        
+        # model metrics
+        val_loss = logs.get("val_loss", [])
+        val_acc = logs.get("val_acc", [])
+        loss = logs.get("loss", [])
+        acc = logs.get("acc", [])
+        
         # checkpoint model at given interval
         if epoch % self.interval == 0:
-            self.model.save(self.path)
+            
+            # frees up memory
+            self.clear_recent_checkpoints(self, os.path.dirname(self.path))
+            
+            self.model.save(str(self.path).format(
+                val_loss=val_loss[-1],
+                val_acc = val_acc[-1],
+                loss=loss[-1],
+                acc=acc[-1],
+                epoch=epoch,
+            ))
+            
+    def clear_recent_checkpoints(self, parent_dir):
+        # get recent checkpoints
+        recent_checkpoint = os.listdir(parent_dir)
+        
+        # delete all recent checkpoints
+        for file in recent_checkpoint:
+            if file.endswith("h5") or file.endswith("hdf5"):
+                os.remove(file)
