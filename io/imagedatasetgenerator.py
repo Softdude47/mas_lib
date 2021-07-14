e06dd3148d4c464595e0df5545ddbeb01bd1ad52
Godwin Precious <godwinprecious47@gmail.com>
2021-07-15 00:31:43 +0100

    fixed bugs in code

--- io/imagedatasetgenerator.py
+++ io/imagedatasetgenerator.py
@@ -4,7 +4,7 @@ import random
 import numpy as np
 from imutils.paths import list_images
 from tensorflow.keras.utils import Sequence
-from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
 
 class ImageDatasetGenerator(Sequence):
     def __init__(self, paths, batch_size=32, preprocessors=None, aug=None, binarize=True, target_size=(224, 224), validation_split=0.2, **kwargs):
@@ -14,9 +14,10 @@ class ImageDatasetGenerator(Sequence):
             `paths`: Parent directory of dataset.
             `batch_size`: Dataset batch size.
             `preprocessors`: Preprocessing functions to apply.
-            `aug`: class that generates augmented images. keras.preprocessing.image.ImagaDataGenerators.
+            `aug`: data augmentation function(or class with `flow` method), accept and returns images in batch
             `binarize`: wether to apply one hot encoding for labels.
-            `target_size`: image size.
+            `target_size`: image `width x height` dimension.
+            `validation_split`: 
         """
         for (key, value) in kwargs.items():
             setattr(self, key, value)
@@ -35,43 +36,96 @@ class ImageDatasetGenerator(Sequence):
         paths = list(list_images(paths))
         random.shuffle(paths)
         self.paths = paths
-        self.labels = [self.get_label(path) for path in self.paths]
-        self.num_classes = len(np.unique(self.labels))
-        print(f"[INFO] Found {len(self.paths)} images... belonging to {self.num_classes} classes")
+        self.num_images = len(paths)
         
+        # encode and store labels
+        self.__lb = LabelBinarizer()
+        self.labels = [self.get_label(path) for path in self.paths]
+        self.labels = self.__lb.fit_transform(self.labels) if self.binarize else self.labels
+        
+        self.classes = [self.get_label(path) for path in self.paths]
+        self.classes = np.unique(self.classes)
+        num_classes = len(self.classes)
+        print(f"[INFO] Found {len(self.paths)} images... belonging to {num_classes} classes")
+        
+        # store generator steps
         steps = len(paths) // batch_size
         self.steps = (steps + 1) if (steps * batch_size) < len(paths) else steps
         
     def __len__(self):
-        # returns number of batches in datasets
+        """
+        Number of batch in the Sequence.
+        # Returns:
+            The number of batches in the Sequence.
+        """
         return self.steps
        
     def __getitem__(self, index):
-        # get the batch at the index-th posiition
+        """
+        Gets batch at position `index`.
+        # Args:
+            `index`: position of the batch in the Sequence.
+        # Returns:
+            A batch of images and corresponding labels each with a size of `self.batch_size`.
+        """
         paths = self.paths[index * self.batch_size : (index + 1) * self.batch_size]
         labels = self.labels[index * self.batch_size : (index + 1) * self.batch_size]
-        labels = to_categorical(labels, num_classes=np.unique(self.labels))
-        images = [self.transform(path) for path in paths]
-    
-        return np.array([images, labels])
+        
+        # load and preprocess image
+        images = [self.transform(path, preprocessors=self.preprocessors) for path in paths]
+        if self.aug is not None:
+            images = next(self.aug.flow(images, batch_size=len(images)))
+        
+        return (np.array(images), labels)
     
     def __get_subset_paths(self, name):
-        # gets train/test data path
+        """
+        Finds list of path under datasets's split name.
+
+        # Args:
+            name: dataset's split name, either `train` or `val`.
+
+        # Returns:
+            List of path under dataset split.
+        """
         if "train" in name:
+            # compute number of train image paths
             split = 1 - self.validation_split
             split = int(split * len(self.paths))
             return self.paths[: split]
+        
+        # compute number of validation image paths
         split = int(self.validation_split * len(self.paths))
         return self.paths[split: ]
     
     def get_label(self, path):
+        """
+        Gets image label from image path.
+
+        # Args:
+            path: full image path.
+
+        # Returns:
+            Encoded label from image path.
+        """
         # extract label from path
+        label_idx = getattr(self, "label_index", -2)
         p = path.replace("/", os.path.sep)
-        label = p.split(os.path.sep)[-2]
+        label = p.split(os.path.sep)[label_idx]
         return label
     
     
     def generate(self, subset="train", passes=np.inf):
+        """
+        Generates image and labels.
+
+        # Args:
+            subset: name of dataset split either `train` or `val`.
+            passes: number of times to iterate.
+
+        # Yields:
+            A tuple with the structure of (`image_batch`, `label_batch`).
+        """
         i = 0
         # generates data
         paths = self.__get_subset_paths(subset)
@@ -84,22 +138,50 @@ class ImageDatasetGenerator(Sequence):
             for (batch_path, batch_labels) in zip(paths, labels):
                 
                 # preprocess each batchs images and labels
-                batch_labels = to_categorical(batch_labels, self.num_classes) if self.binarize else batch_labels
-                batch_images = [self.transform(path, getattr(self, "val_preprocessors", [])) for path in batch_path]
-                if "train" in subset and self.aug is not None:
+                batch_labels = self.__lb.transform(batch_labels, self.num_images) if self.binarize else batch_labels
+                
+                if "val" in subset:
+                    # checks for any preprocessors made for validation image split
+                    val_preprocessors = getattr(self, "val_preprocessors", None)
+                    val_preprocessors = val_preprocessors or getattr(self, "validation_preprocessors", [])
+                    
+                    # loads image and applies preprocesors(if any)
+                    batch_images = [self.transform(path, val_preprocessors) for path in batch_path]
+                
+                if "train" in subset:
+                    # loads image and applies preprocesors(if any)
                     batch_images = [self.transform(path, self.preprocessors) for path in batch_path]
-                    batch_images = self.aug.flow(batch_images, batch_size=self.batch_size)
+                    
+                    # applies data additional augmentation to loaded images
+                    if self.aug is not None:
+                        try:
+                            batch_images = next(self.aug.flow(batch_images, batch_size=self.batch_size))
+                        except:
+                            batch_images = next(self.aug(batch_images, batch_size=self.batch_size))
                     
                 # returns batch
-                yield np.array(batch_images, batch_labels)
+                yield (np.array(batch_images), batch_labels)
             i += 1
         
         
-    def transform(self, path, preprocessors):
-        # load and preprocess an image from path
+    def transform(self, path, preprocessors=[]):
+        """
+        Loads image from `path` and preprocess it with functions inside `preprocessors`.
+
+        # Args:
+            path: valid path to an existing image.
+            preprocessors: list of preprocessing functions(or classes with a `preprocess` method) that accepts and returns an image.
+
+        # Returns:
+            A preprocessed image.
+        """
+        preprocessors = preprocessors or []
+        # load an image from path
         image = cv2.imread(path)
         image = cv2.resize(image, self.target_size)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        # apply preprocessor
         for preprocessor in preprocessors:
             image = preprocessor.preprocess(image)
         
